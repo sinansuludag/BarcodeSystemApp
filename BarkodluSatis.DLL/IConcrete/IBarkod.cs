b2ed68a13b1bcd9c48be305d0690d12a4f44461f
Sinan Suludağ <sinan.sldg23@gmail.com>
2024-10-21 19:23:57 +0300

    BLL,DLL katmanı eklendi IConcrete, COncrete oluşturuldu

---
+++ BarkodluSatis.DLL/IConcrete/IBarkod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkodluSatis.DLL.IConcrete
+{
+    public interface IBarkod
+    {
+        string  BarkodKaydet(string barkodNo);//Camel
+        //Delete, update, list,find, get,....
+
+
+    }
+}
